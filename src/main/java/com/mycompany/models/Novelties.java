cd188a635b445e1cce69f7693bba7317b3d0c987
Cristian-Marcano <cristianmm.1112@gmail.com>
2024-11-13 15:54:34 -0400

    feat(atribute active)

--- src/main/java/com/mycompany/models/Novelties.java
+++ src/main/java/com/mycompany/models/Novelties.java
@@ -7,15 +7,21 @@ package com.mycompany.models;
 public class Novelties {
     private int id;
     private String name;
+    private boolean active;
 
-    public Novelties(int id, String name) {
+    public Novelties(int id, String name, boolean active) {
         this.id = id;
         this.name = name;
+        this.active = active;
     }
     
     //* Setters
     public void setName(String name) {
         this.name = name;
+    }
+    
+    public void setActive(boolean active) {
+        this.active = active;
     }
     
     //* Getters
@@ -25,5 +31,9 @@ public class Novelties {
 
     public String getName() {
         return name;
+    }
+    
+    public boolean getActive() {
+        return active;
     }
 }
