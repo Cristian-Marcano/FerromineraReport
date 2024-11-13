a93e9e816932d3e363c220cc3d0fdac8381c0e70
Cristian-Marcano <cristianmm.1112@gmail.com>
2024-11-13 16:11:56 -0400

    feat(service.NoveltiesService)

---
+++ src/main/java/com/mycompany/service/NoveltiesService.java
@@ -0,0 +1,66 @@
+package com.mycompany.service;
+
+import com.mycompany.DB.Database;
+import com.mycompany.models.Novelties;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Cristian
+ */
+public class NoveltiesService extends Database {
+    
+    public Novelties getNovelties(int id) throws Exception {
+        String sql = "SELECT * FROM novelties WHERE id = ?";
+        applyConnection();
+        statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        result = statement.executeQuery();
+        Novelties novl = null;
+        if(result.next())
+            novl = new Novelties(result.getInt("id"), result.getString("name"), result.getBoolean("active"));
+        closeConnection();
+        return novl;
+    }
+    
+    public List<Novelties> getNovelties() throws Exception {
+        String sql = "SELECT * FROM novelties WHERE active = 1";
+        applyConnection();
+        statement = connection.prepareStatement(sql);
+        result = statement.executeQuery();
+        List<Novelties> listNovelties = new ArrayList<>();
+        if(result.next())
+            listNovelties.add(new Novelties(result.getInt("id"), result.getString("name"), result.getBoolean("active")));
+        closeConnection();
+        return listNovelties;
+    }
+    
+    public void createNovelties(String name) throws Exception {
+        String sql = "INSERT INTO novelties(name) VALUES (?)";
+        applyConnection();
+        statement = connection.prepareStatement(sql);
+        statement.setString(1, name);
+        statement.executeUpdate();
+        closeConnection();
+    }
+    
+    public void updateNovelties(int id, String name) throws Exception {
+        String sql = "UPDATE novelties SET name = ? WHERE id = ?";
+        applyConnection();
+        statement = connection.prepareStatement(sql);
+        statement.setString(1, name);
+        statement.setInt(2, id);
+        statement.executeUpdate();
+        closeConnection();
+    }
+    
+    public void removeNovelties(int id) throws Exception {
+        String sql = "UPDATE novelties SET active = 0 WHERE id = ?";
+        applyConnection();
+        statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        statement.executeUpdate();
+        closeConnection();
+    }
+}
