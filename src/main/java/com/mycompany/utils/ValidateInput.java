bc2f6b178a2ec93b187a43aaebb38c403f6f5425
Cristian-Marcano <cristianmm.1112@gmail.com>
2024-11-15 16:55:04 -0400

    docs(functions)

--- src/main/java/com/mycompany/utils/ValidateInput.java
+++ src/main/java/com/mycompany/utils/ValidateInput.java
@@ -7,9 +7,10 @@ import javax.swing.JTextField;
 /**
  *
  * @author Cristian
+ * Clase que ayuda en la validacion de inputs de las interfaces visuales del proyecto
  */
 public class ValidateInput {
-    
+    //* Valida si el listado de inputs estan vacios y enlista sus nombres en excepciones, para mostrarlo al usuario
     public static void isEmptyOrBlank(List<JTextField> inputs) throws Exception {
         String names = "";
         for(JTextField input: inputs)
@@ -17,12 +18,12 @@ public class ValidateInput {
         
         if(!names.isEmpty()) throw new Exception(names + " estan vacios");
     }
-    
+    //* Valida si el input cumple con el minimo de caracteres
     public static void isMinimumLength(JTextField input, int length) throws Exception {
         if(input.getText().length() < length) throw new Exception(input.getName() + "debe de tener un minimo de " + length + " caracteres");
     }
-    
+    //* Valida si el inputPassword cumple con el minimo de caracteres
     public static void isMinimumLength(JPasswordField input, int length) throws Exception {
-        if(input.getPassword().length < length) throw new Exception(input.getName() + "debe de tener un minimo de " + length + " caracteres");
+        if(input.getPassword().length < length) throw new Exception(input.getName() + " debe de tener un minimo de " + length + " caracteres");
     }
 }
