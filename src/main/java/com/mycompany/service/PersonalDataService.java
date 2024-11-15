05a0ef128f0a5e47b14e82509ef57fbc6a1edac2
Cristian-Marcano <cristianmm.1112@gmail.com>
2024-11-15 15:22:53 -0400

    feat(service.PersonalDataService)

---
+++ src/main/java/com/mycompany/service/PersonalDataService.java
@@ -0,0 +1,37 @@
+package com.mycompany.service;
+
+import com.mycompany.DB.Database;
+import com.mycompany.models.PersonalData;
+
+/**
+ *
+ * @author Cristian
+ */
+public class PersonalDataService extends Database {
+    
+    public void createPersonalData(PersonalData data) throws Exception {
+        String sql = "INSERT INTO personal_data(name, last_name, ficha, tlf, user_id) VALUES (?,?,?,?,?)";
+        applyConnection();
+        statement = connection.prepareStatement(sql);
+        statement.setString(1, data.getName());
+        statement.setString(2, data.getLastName());
+        statement.setString(3, data.getFicha());
+        statement.setString(4, data.getTlf());
+        statement.setInt(5, data.getUserId());
+        statement.executeUpdate();
+        closeConnection();
+    }
+    
+    public void updatePersonalData(PersonalData data) throws Exception {
+        String sql = "UPDATE personal_data SET name = ?, last_name = ?, ficha = ?, tlf = ? WHERE id = ?";
+        applyConnection();
+        statement = connection.prepareStatement(sql);
+        statement.setString(1, data.getName());
+        statement.setString(2, data.getLastName());
+        statement.setString(3, data.getFicha());
+        statement.setString(4, data.getTlf());
+        statement.setInt(5, data.getId());
+        statement.executeUpdate();
+        closeConnection();
+    }
+}
