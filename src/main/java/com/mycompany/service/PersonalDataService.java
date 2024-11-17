846505b7778644f152a9b66561f494bf3388fe6c
Cristian-Marcano <cristianmm.1112@gmail.com>
2024-11-17 19:43:53 -0400

    feat(getPersonalData)

--- src/main/java/com/mycompany/service/PersonalDataService.java
+++ src/main/java/com/mycompany/service/PersonalDataService.java
@@ -10,6 +10,20 @@ import java.sql.SQLException;
  */
 public class PersonalDataService extends Database {
     
+    public PersonalData getPersonalData(int userId) throws SQLException {
+        String sql = "SELECT * FROM personal_data WHERE user_id = ?";
+        applyConnection();
+        statement = connection.prepareStatement(sql);
+        statement.setInt(1, userId);
+        result = statement.executeQuery();
+        PersonalData data = null;
+        if(result.next())
+            data = new PersonalData(result.getInt("id"), result.getInt("user_id"), result.getString("name"),
+                                    result.getString("last_name"), result.getString("ficha"), result.getString("tlf"));
+        closeConnection();
+        return data;
+    }
+    
     public void createPersonalData(PersonalData data) throws SQLException {
         String sql = "INSERT INTO personal_data(name, last_name, ficha, tlf, user_id) VALUES (?,?,?,?,?)";
         applyConnection();
