ba7250e88c3480d400e79885f665f5c4f565599c
Cristian-Marcano <cristianmm.1112@gmail.com>
2024-11-17 20:39:03 -0400

    feat(validate data on ForgotBar)

--- src/main/java/com/mycompany/view/ForgotBar.java
+++ src/main/java/com/mycompany/view/ForgotBar.java
@@ -1,5 +1,15 @@
 package com.mycompany.view;
 
+import com.mycompany.ferromineraproject.FerromineraProject;
+import com.mycompany.models.PersonalData;
+import com.mycompany.models.User;
+import com.mycompany.service.PersonalDataService;
+import com.mycompany.service.UserService;
+import com.mycompany.utils.ValidateInput;
+import java.sql.SQLException;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Cristian
@@ -28,15 +38,12 @@ public class ForgotBar extends javax.swing.JPanel {
         labelLastName = new javax.swing.JLabel();
         separatorLastName = new javax.swing.JSeparator();
         inputLastName = new javax.swing.JTextField();
-        labelUsername = new javax.swing.JLabel();
-        separatorUsername = new javax.swing.JSeparator();
-        inputUsername = new javax.swing.JTextField();
-        labelCI = new javax.swing.JLabel();
-        separatorCI = new javax.swing.JSeparator();
-        inputCI = new javax.swing.JTextField();
         labelFicha = new javax.swing.JLabel();
         separatorFicha = new javax.swing.JSeparator();
         inputFicha = new javax.swing.JTextField();
+        labelUsername = new javax.swing.JLabel();
+        separatorUsername = new javax.swing.JSeparator();
+        inputUsername = new javax.swing.JTextField();
         btnForgot = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(247, 34, 34));
@@ -51,39 +58,20 @@ public class ForgotBar extends javax.swing.JPanel {
         inputName.setBackground(new java.awt.Color(255, 255, 255));
         inputName.setFont(new java.awt.Font("Bahnschrift", 0, 15)); // NOI18N
         inputName.setForeground(new java.awt.Color(50, 50, 50));
+        inputName.setName("Nombre"); // NOI18N
 
         labelLastName.setFont(new java.awt.Font("Bahnschrift", 1, 16)); // NOI18N
         labelLastName.setForeground(new java.awt.Color(255, 255, 255));
         labelLastName.setLabelFor(inputLastName);
         labelLastName.setText("Apellido:");
+        labelLastName.setName(""); // NOI18N
 
         separatorLastName.setForeground(new java.awt.Color(255, 255, 255));
 
         inputLastName.setBackground(new java.awt.Color(255, 255, 255));
         inputLastName.setFont(new java.awt.Font("Bahnschrift", 0, 15)); // NOI18N
         inputLastName.setForeground(new java.awt.Color(50, 50, 50));
-
-        labelUsername.setFont(new java.awt.Font("Bahnschrift", 1, 16)); // NOI18N
-        labelUsername.setForeground(new java.awt.Color(255, 255, 255));
-        labelUsername.setLabelFor(inputUsername);
-        labelUsername.setText("Nombre de Usuario:");
-
-        separatorUsername.setForeground(new java.awt.Color(255, 255, 255));
-
-        inputUsername.setBackground(new java.awt.Color(255, 255, 255));
-        inputUsername.setFont(new java.awt.Font("Bahnschrift", 0, 15)); // NOI18N
-        inputUsername.setForeground(new java.awt.Color(50, 50, 50));
-
-        labelCI.setFont(new java.awt.Font("Bahnschrift", 1, 16)); // NOI18N
-        labelCI.setForeground(new java.awt.Color(255, 255, 255));
-        labelCI.setLabelFor(inputCI);
-        labelCI.setText("Cedula:");
-
-        separatorCI.setForeground(new java.awt.Color(255, 255, 255));
-
-        inputCI.setBackground(new java.awt.Color(255, 255, 255));
-        inputCI.setFont(new java.awt.Font("Bahnschrift", 0, 15)); // NOI18N
-        inputCI.setForeground(new java.awt.Color(50, 50, 50));
+        inputLastName.setName("Apellido"); // NOI18N
 
         labelFicha.setFont(new java.awt.Font("Bahnschrift", 1, 16)); // NOI18N
         labelFicha.setForeground(new java.awt.Color(255, 255, 255));
@@ -95,6 +83,19 @@ public class ForgotBar extends javax.swing.JPanel {
         inputFicha.setBackground(new java.awt.Color(255, 255, 255));
         inputFicha.setFont(new java.awt.Font("Bahnschrift", 0, 15)); // NOI18N
         inputFicha.setForeground(new java.awt.Color(50, 50, 50));
+        inputFicha.setName("Ficha"); // NOI18N
+
+        labelUsername.setFont(new java.awt.Font("Bahnschrift", 1, 16)); // NOI18N
+        labelUsername.setForeground(new java.awt.Color(255, 255, 255));
+        labelUsername.setLabelFor(inputUsername);
+        labelUsername.setText("Nombre de Usuario:");
+
+        separatorUsername.setForeground(new java.awt.Color(255, 255, 255));
+
+        inputUsername.setBackground(new java.awt.Color(255, 255, 255));
+        inputUsername.setFont(new java.awt.Font("Bahnschrift", 0, 15)); // NOI18N
+        inputUsername.setForeground(new java.awt.Color(50, 50, 50));
+        inputUsername.setName("Nombre de usuario"); // NOI18N
 
         btnForgot.setBackground(new java.awt.Color(65, 75, 178));
         btnForgot.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
@@ -118,9 +119,6 @@ public class ForgotBar extends javax.swing.JPanel {
                         .addComponent(labelFicha, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGap(299, 299, 299))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(labelCI, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGap(286, 286, 286))
-                    .addGroup(layout.createSequentialGroup()
                         .addComponent(labelUsername, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGap(192, 192, 192))
                     .addGroup(layout.createSequentialGroup()
@@ -135,8 +133,6 @@ public class ForgotBar extends javax.swing.JPanel {
                     .addComponent(inputLastName)
                     .addComponent(separatorUsername)
                     .addComponent(inputUsername)
-                    .addComponent(separatorCI)
-                    .addComponent(inputCI)
                     .addComponent(separatorFicha)
                     .addComponent(inputFicha))
                 .addGap(30, 30, 30))
@@ -161,12 +157,6 @@ public class ForgotBar extends javax.swing.JPanel {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(inputLastName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(labelCI)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(separatorCI, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(inputCI, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(labelFicha)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(separatorFicha, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -178,32 +168,64 @@ public class ForgotBar extends javax.swing.JPanel {
                 .addComponent(separatorUsername, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(inputUsername, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(54, 54, 54)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 133, Short.MAX_VALUE)
                 .addComponent(btnForgot, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(198, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    //* Funcion que se acciona cuando se presiona el btnForgot
     //* Valida los input si se estan usando y verifica en la DB si se encuentra el usuario
     private void btnForgotActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnForgotActionPerformed
-        // TODO add your handling code here:
+        try {
+            ValidateInput.isEmptyOrBlank(List.of(inputName, inputLastName, inputFicha, inputUsername));
+            
+            String name = inputName.getText(), lastName = inputLastName.getText(),
+                    ficha = inputFicha.getText(), username = inputUsername.getText();
+            
+            UserService userService = new UserService();
+            User user = userService.getUser(username);
+            
+            if(user == null) throw new Exception("Datos invalidos");
+            
+            PersonalDataService dataService = new PersonalDataService();
+            PersonalData data = dataService.getPersonalData(user.getId());
+            
+            if(user.getUsername().equals(username) && data.getName().equals(name) && data.getLastName().equals(lastName) && data.getFicha().equals(ficha)) {
+                
+                FerromineraProject.user = user;
+                
+                if(user.getRole().equals("super-admin")) FerromineraProject.board.setPanel(new MenuAdminBar());
+                else FerromineraProject.board.setPanel(new MenuBar());
+                
+                FerromineraProject.contentP.setPanel(new ScrollReportContent());
+                FerromineraProject.contentP.showPanel();
+                
+                FerromineraProject.board.showPanel();
+                
+                ScrollReportContent.initReportContent(10, 0);
+                
+            } else throw new Exception("Datos invalidos");
+            
+        } catch(SQLException e) {
+            System.err.println(e.getMessage());
+            JOptionPane.showMessageDialog(null,"Ocurrio un Error en la conexion con la Base de Datos","ERROR",JOptionPane.ERROR_MESSAGE);
+        } catch(Exception e) {
+            System.err.println(e.getMessage());
+            JOptionPane.showMessageDialog(null,"No se puede avanzar \n" + e.getMessage(),"Advertencia",JOptionPane.WARNING_MESSAGE);
+        }
     }//GEN-LAST:event_btnForgotActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnForgot;
-    private javax.swing.JTextField inputCI;
     private javax.swing.JTextField inputFicha;
     private javax.swing.JTextField inputLastName;
     private javax.swing.JTextField inputName;
     private javax.swing.JTextField inputUsername;
-    private javax.swing.JLabel labelCI;
     private javax.swing.JLabel labelFicha;
     private javax.swing.JLabel labelLastName;
     private javax.swing.JLabel labelName;
     private javax.swing.JLabel labelUsername;
-    private javax.swing.JSeparator separatorCI;
     private javax.swing.JSeparator separatorFicha;
     private javax.swing.JSeparator separatorLastName;
     private javax.swing.JSeparator separatorName;
