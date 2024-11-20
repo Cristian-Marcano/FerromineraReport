e5b4c45ee3eb6053ff760703a7c35b044b8a6a5d
Cristian-Marcano <cristianmm.1112@gmail.com>
2024-11-20 18:54:00 -0400

    fix(change List<JTextField> to List<JTextComponent> )

--- src/main/java/com/mycompany/utils/ValidateInput.java
+++ src/main/java/com/mycompany/utils/ValidateInput.java
@@ -3,6 +3,7 @@ package com.mycompany.utils;
 import java.util.List;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.text.JTextComponent;
 
 /**
  *
@@ -10,18 +11,21 @@ import javax.swing.JTextField;
  * Clase que ayuda en la validacion de inputs de las interfaces visuales del proyecto
  */
 public class ValidateInput {
+    
     //* Valida si el listado de inputs estan vacios y enlista sus nombres en excepciones, para mostrarlo al usuario
-    public static void isEmptyOrBlank(List<JTextField> inputs) throws Exception {
+    public static void isEmptyOrBlank(List<JTextComponent> inputs) throws Exception {
         String names = "";
-        for(JTextField input: inputs)
+        for(JTextComponent input: inputs)
             if(input.getText().isEmpty() || input.getText().isBlank()) names += (names.isEmpty()) ? input.getName() : ", " + input.getName();
         
         if(!names.isEmpty()) throw new Exception(names + " estan vacios");
     }
+    
     //* Valida si el input cumple con el minimo de caracteres
     public static void isMinimumLength(JTextField input, int length) throws Exception {
         if(input.getText().length() < length) throw new Exception(input.getName() + "debe de tener un minimo de " + length + " caracteres");
     }
+    
     //* Valida si el inputPassword cumple con el minimo de caracteres
     public static void isMinimumLength(JPasswordField input, int length) throws Exception {
         if(input.getPassword().length < length) throw new Exception(input.getName() + " debe de tener un minimo de " + length + " caracteres");
