2307266d2f3968853d312c82c6aba73611d585fd
Cristian-Marcano <cristianmm.1112@gmail.com>
2024-11-22 18:04:14 -0400

    fix(added commit and removed applyConnection in (createPersonalData and updatePersonalData))

--- src/main/java/com/mycompany/service/PersonalDataService.java
+++ src/main/java/com/mycompany/service/PersonalDataService.java
@@ -26,7 +26,6 @@ public class PersonalDataService extends Database {
     
     public void createPersonalData(PersonalData data) throws SQLException {
         String sql = "INSERT INTO personal_data(name, last_name, ficha, tlf, user_id) VALUES (?,?,?,?,?)";
-        applyConnection();
         statement = connection.prepareStatement(sql);
         statement.setString(1, data.getName());
         statement.setString(2, data.getLastName());
@@ -34,12 +33,12 @@ public class PersonalDataService extends Database {
         statement.setString(4, data.getTlf());
         statement.setInt(5, data.getUserId());
         statement.executeUpdate();
+        connection.commit();
         closeConnection();
     }
     
     public void updatePersonalData(PersonalData data) throws SQLException {
         String sql = "UPDATE personal_data SET name = ?, last_name = ?, ficha = ?, tlf = ? WHERE id = ?";
-        applyConnection();
         statement = connection.prepareStatement(sql);
         statement.setString(1, data.getName());
         statement.setString(2, data.getLastName());
@@ -47,6 +46,7 @@ public class PersonalDataService extends Database {
         statement.setString(4, data.getTlf());
         statement.setInt(5, data.getId());
         statement.executeUpdate();
+        connection.commit();
         closeConnection();
     }
 }
